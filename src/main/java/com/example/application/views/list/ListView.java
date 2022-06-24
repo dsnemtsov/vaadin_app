085be9ea89724c9c8a29cc3e52c0449b0791397b
d.s.nemtsov <dimas224@yandex.ru>
2022-06-24 16:28:00 +0300

    SP

--- src/main/java/com/example/application/views/list/ListView.java
+++ src/main/java/com/example/application/views/list/ListView.java
@@ -1,9 +1,10 @@
 package com.example.application.views.list;
 
-import com.vaadin.flow.component.html.H2;
-import com.vaadin.flow.component.html.Image;
-import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
@@ -12,19 +13,15 @@ import com.vaadin.flow.router.Route;
 public class ListView extends VerticalLayout {
 
     public ListView() {
-        setSpacing(false);
+        Button button = new Button("Click me");
+        TextField name = new TextField("Name");
 
-        Image img = new Image("images/empty-plant.png", "placeholder plant");
-        img.setWidth("200px");
-        add(img);
+        HorizontalLayout hl = new HorizontalLayout(name, button);
+        hl.setDefaultVerticalComponentAlignment(Alignment.BASELINE);
 
-        add(new H2("This place intentionally left empty"));
-        add(new Paragraph("It’s a place where you can grow your own UI 🤗"));
+        button.addClickListener(click -> Notification.show("Hello, " + name.getValue()));
 
-        setSizeFull();
-        setJustifyContentMode(JustifyContentMode.CENTER);
-        setDefaultHorizontalComponentAlignment(Alignment.CENTER);
-        getStyle().set("text-align", "center");
+        add(hl);
     }
 
 }
